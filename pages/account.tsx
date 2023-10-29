d5b511536a26c0d13892840fd3d52da6a34d7ac7
nknighta <n.knight.pc0627@gmail.com>
2023-10-29 21:32:54 +0900

    feat: add account page

---
+++ pages/account.tsx
@@ -0,0 +1,10 @@
+import ETHBalance from "../components/ETHBalance";
+
+export default function AccountPage () {
+    return (
+        <div>
+            <h1>Account</h1>
+            <ETHBalance />
+        </div>
+    );
+}
